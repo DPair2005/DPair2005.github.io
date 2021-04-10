21e03e745e9abbec8bb3a00e4c8a1e36180dc038
DPair <DPair2005@163.com>
2021-04-10 19:47:30 +0800

    AKIOI

--- js/element.js
+++ js/element.js
@@ -92,7 +92,9 @@ function drawSaying() {
         '“很遗憾，被我卡掉了。” —— lxl',
         '意☆义☆不☆明的卡牌',
         'I am iron man.',
-        '“基本上所有单点修改都可以强化成区间推平。” —— lxl'
+        '“基本上所有单点修改都可以强化成区间推平。” —— lxl',
+        '“分块需要带活？” —— lxl',
+        '“哦对了忘了说题解了” —— lxl',
     ]
     document.getElementById("saying").innerText = says[rand(says.length)]
 }
