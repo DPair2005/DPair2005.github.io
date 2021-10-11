08fe8f9ce3c956888701fd69c04a1b46f43e032b
DPair <dpair2005@163.com>
2021-10-11 20:00:40 +0800

    2021年 10月 11日 星期一 20:00:39 CST

--- js/fun.js
+++
@@ -1,8 +0,0 @@
-(function (win) {
-    'use strict';
-    let winbody = win.querySelector('body');
-    if (win.Funval == 'MYYAKAK')
-        winbody.style.setProperty("--fun-display", 'default');
-    else 
-        winbody.style.setProperty("--fun-display", 'none');
-})(document);
