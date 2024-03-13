576338fd826c8f5167a389af5d94865e9a322060
DPair <dpair2005@163.com>
2024-03-13 14:19:59 +0800

    $DATE

--- js/hitokoto.js
+++ js/hitokoto.js
@@ -7,12 +7,15 @@ fetch('https://v1.hitokoto.cn?&c=d&c=e&c=h&c=i&c=k')
     
     // hitokoto.href = `https://hitokoto.cn/?uuid=${data.uuid}`
     document.getElementById("sayingtype").innerText = '一言（hitokoto）'
-    if (data.from_who.length + data.from.length >= 13) data.from = null;
-    if (data.from_who != null && data.from != null)
+    if (data.from == null) data.from = '';
+    if (data.from_who == null) data.from_who = '';
+
+    if (data.from_who.length + data.from.length >= 13) data.from = '';
+    if (data.from_who != '' && data.from != '')
         document.getElementById('sayer').innerText = '——' + data.from_who + '《' + data.from + '》'
-    else if (data.from != null)
+    else if (data.from != '')
         document.getElementById('sayer').innerText = '——《' + data.from + '》'
-    else if (data.from_who != null)
+    else if (data.from_who != '')
         document.getElementById('sayer').innerText = '——' + data.from_who
     else {
         document.getElementById('sayer').innerText = ''
